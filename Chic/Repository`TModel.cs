5962e4a034cfd9c0a7266102f88805dc2586e8a0
Rudi Visser <rudi@cedita.com>
2018-05-15 18:15:41 +0100

    Fix IRepository<T> also

--- Chic/Repository`TModel.cs
+++ Chic/Repository`TModel.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Cedita Ltd. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the solution root for license information.
 using System.Data;
+using Chic.Abstractions;
 using Chic.Constraints;
 
 namespace Chic
 {
-    public class Repository<TModel> : Repository<TModel, int>
+    public class Repository<TModel> : Repository<TModel, int>, IRepository<TModel>
         where TModel : class, IKeyedEntity
     {
         public Repository(IDbConnection db) : base(db)
