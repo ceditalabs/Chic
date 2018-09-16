050880ef97c7832563dc2968c9e7bfbe1cd00c45
Rudi Visser <rudi@cedita.com>
2018-09-16 19:15:27 +0100

    Identity Builder Extension

---
+++ Chic.Identity/IdentityChicBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Cedita Ltd. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the solution root for license information.
+using Chic.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IdentityChicBuilderExtensions
+    {
+        public static IdentityBuilder AddChicStores(this IdentityBuilder builder)
+        {
+            AddStores(builder.Services, builder.UserType, builder.RoleType);
+            return builder;
+        }
+
+        private static void AddStores(IServiceCollection services, Type userType, Type roleType)
+        {
+            var identityUserType = FindGenericBaseType(userType, typeof(IdentityUser<>));
+            if (identityUserType == null)
+            {
+                throw new InvalidOperationException("Type is not an Identity User");
+            }
+
+            var keyType = identityUserType.GenericTypeArguments[0];
+
+            var identityRoleType = FindGenericBaseType(roleType, typeof(IdentityRole<>));
+            if (identityRoleType == null)
+            {
+                throw new InvalidOperationException("Type is not an Identity Role");
+            }
+
+            var userStoreType = typeof(UserStore<,,>).MakeGenericType(userType, roleType, keyType);
+            var roleStoreType = typeof(RoleStore<,>).MakeGenericType(roleType, keyType);
+
+            services.TryAddScoped(typeof(IUserStore<>).MakeGenericType(userType), userStoreType);
+            services.TryAddScoped(typeof(IRoleStore<>).MakeGenericType(roleType), roleStoreType);
+        }
+
+        private static TypeInfo FindGenericBaseType(Type currentType, Type genericBaseType)
+        {
+            var type = currentType;
+            while (type != null)
+            {
+                var typeInfo = type.GetTypeInfo();
+                var genericType = type.IsGenericType ? type.GetGenericTypeDefinition() : null;
+                if (genericType != null && genericType == genericBaseType)
+                {
+                    return typeInfo;
+                }
+                type = type.BaseType;
+            }
+            return null;
+        }
+    }
+}
